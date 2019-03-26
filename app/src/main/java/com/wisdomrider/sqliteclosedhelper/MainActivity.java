540fbc03ea2683fd73fac57723b83099f97b43af
avishek <avishekzone@gmail.com>
2019-03-26 20:08:33 +0545

    Writing readme

--- app/src/main/java/com/wisdomrider/sqliteclosedhelper/MainActivity.java
+++ app/src/main/java/com/wisdomrider/sqliteclosedhelper/MainActivity.java
@@ -13,18 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SqliteClosedHelper closed = new SqliteClosedHelper(this, "DBNAME");
+        SqliteClosedHelper helper = new SqliteClosedHelper(this, "DBNAME");
 
 
 
 
 
 
-//        closed.updateTable(c);
-//        ArrayList<Checks> checks = closed.whereOR(c);
-
-
-//        Log.e(checks.toString(), checks.get(0).id + "-" + checks.get(0).age + "=" + checks.get(0).place + "-" + checks.get(0).name + "-" + checks.get(0).hey);
     }
 
 }
